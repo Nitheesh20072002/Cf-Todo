a1021158183c39a57e3d0419e040f2d931a36859
Nitheesh kumar <nitheeshkumar.chapala@gmail.com>
2022-05-20 12:20:59 +0530

    adding mongo models

---
+++ models/user.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema=mongoose.Schema;
+
+const userSchema= new Schema({
+    handle: String,
+    following: [String],
+    followers: [String],
+    submissions:[
+        {
+            problemId: String,
+            problemName:String,
+            problemStatus: String,
+        },
+    ],
+    todo:[
+        {
+            handle:String,
+            problemId: String,
+            problemName:String,
+            problemStatus: String,
+        }
+    ],
+});
+
+export default mongoose.model('User',userSchema);
